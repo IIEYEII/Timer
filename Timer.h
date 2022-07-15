8d8f8024eeb0160b40ba08af7b2ebd753499518b
ParkSeHyeon <91875380+JIGUMICS@users.noreply.github.com>
2022-07-15 19:17:31 +0900

    Update Timer.h

--- Timer.h
+++ Timer.h
@@ -2,11 +2,6 @@
 #include <iostream>
 #include <chrono>
 
-/* How to use
-* 시간측정 시작 위치에 생성자 클래스 생성 Timer timer;
-* 시간측정 종료 위치에 timer.elapsed(); 호출
-*/
-
 class Timer
 {
 	using clock_t = std::chrono::high_resolution_clock;
